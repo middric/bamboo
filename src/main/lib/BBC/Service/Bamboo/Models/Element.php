4e7b383b534b900c632ec8b754c608d02aa2bb35
Miguel Gonzalez <miguel.gonzalez@bbc.co.uk>
2013-08-21 14:04:13 +0000

    Ability to have non-numeric image recipes

--- src/main/lib/BBC/Service/Bamboo/Models/Element.php
+++ src/main/lib/BBC/Service/Bamboo/Models/Element.php
@@ -56,15 +56,15 @@ class BBC_Service_Bamboo_Models_Element extends BBC_Service_Bamboo_Models_Base
     /**
      * Get the standard image url for an element
      *
-     * @param int $width  Desired width of image (default 336)
+     * @param string|int $width  Desired width of image (default 336).
+     *                           if not an integer indictes a recipe name
      * @param int $height Desired height of image (default 189)
      *
      * @return string
      */
     public function getStandardImage($width = 336, $height = 189) {
-        $dimensions = "{$width}x{$height}";
         if (isset($this->_images['standard'])) {
-            return str_replace('{recipe}', $dimensions, $this->_images['standard']);
+            return str_replace('{recipe}', $this->_getRecipe($width, $height), $this->_images['standard']);
         }
         return "";
     }
@@ -84,15 +84,15 @@ class BBC_Service_Bamboo_Models_Element extends BBC_Service_Bamboo_Models_Base
     /**
      * Get the portrait image url for an element
      *
-     * @param int $width  Desired width of image (default 336)
+     * @param string|int $width  Desired width of image (default 336).
+     *                           if not an integer indicates a recipe name
      * @param int $height Desired height of image (default 581)
      *
      * @return string
      */
     public function getPortraitImage($width = 336, $height = 581) {
-        $dimensions = "{$width}x{$height}";
         if (isset($this->_images['portrait'])) {
-            return str_replace('{recipe}', $dimensions, $this->_images['portrait']);
+            return str_replace('{recipe}', $this->_getRecipe($width, $height), $this->_images['portrait']);
         }
         return "";
     }
@@ -113,22 +113,22 @@ class BBC_Service_Bamboo_Models_Element extends BBC_Service_Bamboo_Models_Base
      * Get the $type image url for an element
      *
      * @param string $type Type of image to get (standard|vertical|portrait)
-     * @param int $width  Desired width of image (default 336)
+     * @param string|int $width  Desired width of image (default 336).
+     *                           if not an integer indicates a recipe name
      * @param int $height Desired height of image (default 581)
      *
      * @return string
      */
     public function getImage($type = 'standard', $width = 336, $height = 581) {
-        $dimensions = "{$width}x{$height}";
         if (isset($this->_images[$type])) {
-            return str_replace('{recipe}', $dimensions, $this->_images[$type]);
+            return str_replace('{recipe}', $this->_getRecipe($width, $height), $this->_images[$type]);
         }
         return "";
     }
 
     /**
      * Get the raw $type image for an element. This is a url which includes the recipe
-     * 
+     *
      * @param string $type Type of image to get (standard|vertical|portrait)
      * @return string
      */
@@ -174,6 +174,19 @@ class BBC_Service_Bamboo_Models_Element extends BBC_Service_Bamboo_Models_Base
      */
     public function getType() {
         return $this->_type;
+    }
+
+    /**
+     * Returns the recipe for a specific width x height or a recipe name
+     *
+     * @param string|int $width  Desired width of image
+     *                           if not an integer it's a recipe name
+     * @param int $height Desired height of image
+     * @access private
+     * @return void
+     */
+    private function _getRecipe($width, $height) {
+        return is_numeric($width) ? "{$width}x{$height}":$width;
     }
 
 }
