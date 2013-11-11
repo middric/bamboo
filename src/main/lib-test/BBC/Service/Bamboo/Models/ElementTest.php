9a0e8839b54842012f22e9e29f3f9a3c689fc931
Craig Taub <craigtaub@bbc.co.uk>
2013-11-11 17:12:48 +0000

    Added test for ElementTest

---
+++ src/main/lib-test/BBC/Service/Bamboo/Models/ElementTest.php
@@ -0,0 +1,102 @@
+<?php 
+class BBC_Service_Bamboo_Models_ElementTest extends PHPUnit_Framework_TestCase
+{
+    protected $_responseEpisode;
+
+    protected function setUp() {
+        $response = Zend_Http_Response::fromString(
+            file_get_contents(dirname(__FILE__) . '/../../../../fixtures/episodes_p01b2b5c.json')
+        );
+        $responseObject = json_decode($response->getBody());
+        $this->_responseEpisode = $responseObject->episodes[0];
+    }
+
+    /* 
+     * Using an Element Mock
+     */
+    public function testGetType() {
+        $mockedElement = $this->_mockElement();
+
+        $this->assertEquals($mockedElement->getType(), 'episode_large');
+    }
+
+    public function testGetShortSynopsis() {
+        $mockedElement = $this->_mockElement(); 
+        $shortSynopsis = 'Luther investigates two horrific cases, unaware his every step is under scrutiny.';
+
+        $this->assertEquals($mockedElement->getShortSynopsis(), $shortSynopsis);
+    }
+
+    public function testGetMasterBrand() {
+        $mockedElement = $this->_mockElement(); 
+
+        $this->assertEquals($mockedElement->getMasterBrand(), 'BBC Two');
+    }
+
+    public function testGetImage() {
+        $mockedElement = $this->_mockElement(); 
+
+        $imageUrl = 'http://ichef.live.bbci.co.uk/images/ic/336x581/legacy/episode/p01b2b5c.jpg';
+        $this->assertEquals($mockedElement->getImage(), $imageUrl);
+    }
+
+    /* 
+     * Using an Episode Mock and inheritance
+     */
+    public function testGetEpisodeType() {
+        $mockedEpisode = $this->_mockEpisode(); 
+
+        $this->assertEquals($mockedEpisode->getType(), 'episode_large');
+    }
+
+    public function testGetEpisodeMasterBrandAttribution() {
+        $mockedEpisode = $this->_mockEpisode(); 
+
+        $this->assertEquals($mockedEpisode->getMasterBrandAttribution(), 'bbc_two');
+    }
+
+    public function testGetEpisodeImageRecipe() {
+        $mockedEpisode = $this->_mockEpisode(); 
+
+        $this->assertEmpty($mockedEpisode->getImageRecipe('vertical'));
+    }
+
+    private function _mockEpisode() {
+        return new EpisodeMock($this->_responseEpisode);
+    }
+
+    private function _mockElement() {
+        return new BBC_Service_Bamboo_Models_Element($this->_responseEpisode);
+    }
+}
+
+class EpisodeMock extends BBC_Service_Bamboo_Models_Element
+{
+    protected $_subtitle = "";
+    // @codingStandardsIgnoreStart
+    protected $_release_date = "";
+    protected $_tleo_id = "";
+    // @codingStandardsIgnoreEnd
+    protected $_versions = array();
+    protected $_film = false;
+    protected $_duration = "";
+    protected $_href = "";
+    protected $_labels = array();
+    protected $_stacked = "";
+    protected $_guidance = "";
+    protected $_credits = "";
+
+    public function getSubtitle() {
+        return $this->_subtitle;
+    }
+
+    public function getTleoId() {
+        // @codingStandardsIgnoreStart
+        return $this->_tleo_id;
+        // @codingStandardsIgnoreStart
+    }
+
+    public function getVersions() {
+        return $this->_versions;
+    }
+}
